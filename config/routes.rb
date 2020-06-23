0ee3fff94a127d828a0b761d888c6343544eb45a
Alexander Eklund <alexek1987@icloud.com>
2020-06-23 15:39:01 +0700

    i added so much stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #1 READ ALL TASKS
-  get 'tasks', to: "tasks#index"
-  #2 READ ONE TASK
-  get 'tasks/:id', to: "tasks#show"
+  # 1 READ ALL TASKS
+  # get 'tasks', to: "tasks#index"
+  # # 2 ADD NEW TASK
+  # get 'tasks/new', to: "tasks#new"
+  # post 'tasks', to: "tasks#create"
 
+  # # 3 READ ONE TASK
+  # get 'tasks/:id', to: "tasks#show", as: :task
+  # #4 updae
+  # get 'tasks/:id/edit', to: "tasks#edit", as: :edit_task
+  # patch 'tasks/:id', to: "tasks#update"
+  # #4 DELETE
+  # delete 'tasks/:id', to: "tasks#destroy"
+  resources :tasks
 end
