6997fa527dd896142b91b948adf65b73bfdf124d
Alexander Eklund <alexek1987@icloud.com>
2020-06-23 12:01:11 +0700

    added tasks controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #1 READ ALL TASKS
+  get 'tasks', to: "tasks#index"
+  #2 READ ONE TASK
+  get 'tasks/:id', to: "tasks#show"
+
 end
